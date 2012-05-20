6c89941eee478e7785c9084efd5fb431fddfb70e
Tim Gross <gross.timothy@gmail.com>
2012-05-20 15:29:27 -0400

    Sketched out registration process.

--- www/registration_backend/forms.py
+++ www/registration_backend/forms.py
@@ -12,11 +12,70 @@ class RegistrationForm(BootstrapForm):
                      "first_name",
                      "last_name",
                      "email_address",
+                     'password',
+                     'password_verify',
                      "accepts_terms"),
             )
 
-    first_name = forms.CharField(widget = forms.Textarea(attrs = {'rows' : 1, 'class' : 'span8'}))
-    last_name = forms.CharField(widget = forms.Textarea(attrs = {'rows' : 1, 'class' : 'span8'}))
-    email_address = forms.EmailField(widget = forms.Textarea(attrs = {'rows' : 1, 'class' : 'span8'}))
-    accepts_terms = forms.BooleanField(required = True)
+    first_name = forms.CharField( label = 'First Name',
+                                  initial = 'Enter your first name',
+                                  widget = forms.Textarea(attrs = {'rows' : 1, 'class' : 'span8'}),
+                                  help_text = 'Your first name, up to 30 characters',
+                                  error_messages = { 'required': 'You must supply your first name.',
+                                                     'max_length': 'A maximum of 30 characters, please.'},
+                                  max_length=30)
+
+    last_name = forms.CharField( label = 'Last Name',
+                                  initial = 'Enter your last name',
+                                  widget = forms.Textarea(attrs = {'rows' : 1, 'class' : 'span8'}),
+                                  help_text = 'Your last name, up to 30 characters',
+                                  error_messages = { 'required': 'You must supply your last name.',
+                                                     'max_length': 'A maximum of 30 characters, please.'},
+                                  max_length=30)
+
+    email_address = forms.EmailField( label = 'Email Address',
+                                      initial = 'Enter your email address',
+                                      widget = forms.Textarea(attrs = {'rows' : 1, 'class' : 'span8'}),
+                                      help_text = 'Your valid email address, up to 200 characters.',
+                                      error_messages = { 'required': 'You must supply an email address.',
+                                                         'invalid': 'That does not appear to be a valid email address.',
+                                                         'max_length': 'A maximum of 200 characters, please.'},
+                                      max_length=200)
+
+    password = forms.CharField(label = 'Password',
+                               initial = 'Enter a password',
+                               widget = forms.PasswordInput(attrs = {'rows' : 1, 'class' : 'span8'}),
+                               help_text = 'Pick a strong password between 8 and 30 characters long.',
+                               error_messages={'required': 'You must choose a password',
+                                               'max_length':
+                                               'That is an awfully long password!  A max of 30 characters, please!',
+                                               'min_length':
+                                               'Choose a longer password.  A minimum of 8 characters, please.'},
+                               min_length=8,
+                               max_length=30)
+
+    password_verify = forms.CharField(label = 'Verify Password',
+                                      initial = 'Verify your password',
+                                      widget = forms.PasswordInput(attrs = {'rows' : 1, 'class' : 'span8'}),
+                                      help_text = 'Re-enter your password.',
+                                      error_messages={'required': 'You must enter your password twice.'},
+                                      max_length=30)
+
+    accepts_terms = forms.BooleanField(label='I accept the Terms and Conditions',
+                                       help_text = 'See the Terms and Conditions on this page.',
+                                       error_messages={'required': 'You must accept the Terms and Conditions'})
+
+    def clean_password(self):
+        '''
+        Verifies that the password matches the user-provided verfication and ensures it has appropriate
+        complexity.
+        '''
+        candidate = self.data['password']
+        if candidate != self.data['password_verify']:
+            raise forms.ValidationError('Your passwords don\'t match.  Please try again.')
+
+        # TODO: need complexity calculation here
+        return self.data['password']
+
+
 
--- www/registration_backend/urls.py
+++ www/registration_backend/urls.py
@@ -2,10 +2,18 @@ from django.conf.urls import patterns, url
 from django.views.generic.simple import direct_to_template
 
 from registration.views import activate, register
+import registration_backend
+from registration_backend import forms
 
 '''
 The django-registration URL configuration has been reimplemented here so that we can override
 the URLs, forms, and templates with ones of our choosing.
+
+Registration workflow is:
+register/       The user requests a scan and we take his name and email address.
+registered/   After form validation, we tell the user to await an email from us.
+activate/      The link the user clicks on from the email we send.
+activated/    We have verified the user and queue the first scan.
 
 Validation note from django-registration:
 Activation keys get matched by \w+ instead of the more specific [a-fA-F0-9]{40} because a bad
@@ -14,26 +22,28 @@ instead of a confusing 404.
 '''
 
 urlpatterns = patterns('',
-                       url(r'^activate/complete/$',
+                       url(r'^register/$',
+                           register,
+                           {'backend': 'registration_backend.AivsBackend',
+                            'success_url': 'registered/',
+                            'form_class': forms.RegistrationForm,
+                            'disallowed_url': 'closed/',
+                            'template_name': 'registration_form.html'},
+                           name='registration_register'),
+                       url(r'^registered/$',
                            direct_to_template,
-                           {'template': 'registration/activation_complete.html'},
+                           {'template': 'registered.html'},
+                           name='registration_complete'),
+                       url(r'^activated/$',
+                           direct_to_template,
+                           {'template': 'activated.html'},
                            name='registration_activation_complete'),
                        url(r'^activate/(?P<activation_key>\w+)/$',
                            activate,
                            {'backend': 'registration_backend.AivsBackend'},
                            name='registration_activate'),
-                       url(r'^register/$',
-                           register,
-                           {'backend': 'registration_backend.AivsBackend',
-                            'form_class': 'aivs.forms.RegistrationForm',
-                            'template': 'submit_scan_form.html'},
-                           name='registration_register'),
-                       url(r'^register/complete/$',
+                       url(r'^closed/$',
                            direct_to_template,
-                           {'template': 'registration/registration_complete.html'},
-                           name='registration_complete'),
-                       url(r'^register/closed/$',
-                           direct_to_template,
-                           {'template': 'registration/registration_closed.html'},
+                           {'template': 'closed.html'},
                            name='registration_disallowed'),
 )
