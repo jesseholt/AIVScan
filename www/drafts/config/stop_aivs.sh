a4fe446070c99d61b5dc1f785d291d97360dae7a
Tim Gross <gross.timothy@gmail.com>
2012-05-31 21:20:27 -0400

    Fixed copy mixup.

--- www/drafts/config/stop_aivs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# hacky way to find the PIDs for gunicorn_django and kill the processes.
-
-kill -9 `ps -e | grep gunicorn_django | awk '{print $1}'`
--- www/drafts/www/aivs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from aivs.models import ScanResult
-
-'''
-This module registers our ORM classes with the Django admin.
-'''
-
-admin.site.register(ScanResult)
