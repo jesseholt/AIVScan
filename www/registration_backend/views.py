6dfb292198c9ea807fe6cdc287b40c4143487c69
Tim Gross <gross.timothy@gmail.com>
2012-07-28 16:27:28 -0400

    New modals for password reset/change.

--- www/registration_backend/views.py
+++ www/registration_backend/views.py
@@ -33,3 +33,32 @@ def login(request):
     return auth_views.login(request,
                             template_name = template_name,
                             authentication_form = forms.AivsAuthenticationForm)
+
+def forgot_password(request):
+    '''
+    Wraps the django.contrib.auth reset_password view, so as to allow for different templates to be
+    set in that view depending on whether or not the request is Ajax.
+    '''
+    if request.is_ajax():
+        template_name = 'forgot_password_modal.html'
+    else:
+        template_name = 'forgot_password.html'
+
+    return auth_views.password_reset(request,
+                                     template_name = template_name,
+                                     post_reset_redirect = 'profile/')
+
+
+def change_password(request):
+    '''
+    Wraps the django.contrib.auth reset_password view, so as to allow for different templates to be
+    set in that view depending on whether or not the request is Ajax.
+    '''
+    if request.is_ajax():
+        template_name = 'password_change_modal.html'
+    else:
+        template_name = 'password_change.html'
+
+    return auth_views.password_change(request,
+                                     template_name = template_name,
+                                     post_change_redirect = 'profile/')
