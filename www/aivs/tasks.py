937fb48d8fc9f9cccf380c562cebdc7ef617a187
Tim Gross <gross.timothy@gmail.com>
2012-08-20 21:22:37 -0400

    subprocess call for task queue

--- www/aivs/tasks.py
+++ www/aivs/tasks.py
@@ -1,10 +1,17 @@
+import os
+import subprocess
+
+from django.conf import settings
 from celery.task import task
 
 @task
-def run_scan(user, safe_ip_address, subscription_level, scan_options, scan_running):
+def run_scan(user, safe_ip_address, subscription_level=0):
     '''
     Executes the scan task.  If this function is called with run_scan.delay(safe_ip_address), then
     it will be placed into the Celery queue for asynchronous processing.
     IMPORTANT: The caller is responsible for validating cleaning the arguments passed to this task!
     '''
-    return 'Done!'
+    pycmd = os.path.abspath(os.path.join(settings.ROOTDIR, '..', 'scanner', 'launcher.py'))
+    subprocess.call([pycmd, safe_ip_address, str(user.id), str(subscription_level)])
+    return
+
