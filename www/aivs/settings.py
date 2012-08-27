107a8b197fa5df906f10a020bb7f9c2a87bdbf4a
Tim Gross <gross.timothy@gmail.com>
2012-08-26 21:56:44 -0400

    Suppressed TZ warnings and removed extraneous context processors.

--- www/aivs/settings.py
+++ www/aivs/settings.py
@@ -38,7 +38,7 @@ USE_I18N = False
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False # HACK: suppresses a warning on the scanner
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = os.path.join(BASEDIR, 'media/')
@@ -84,10 +84,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.request',
-    'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
-    'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     )
 
