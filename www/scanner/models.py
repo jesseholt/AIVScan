3c3cc8caa5d49229783844a20a7c7329b33a0aea
Tim Gross <gross.timothy@gmail.com>
2012-08-27 20:28:34 -0400

    scanner.model updates to suit data import and current views.

--- www/scanner/models.py
+++ www/scanner/models.py
@@ -1,143 +1,185 @@
 # -*- coding: utf-8 -*-
 # Copyright 2012 Team Pwn Stars
 
+import datetime
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
 
 '''
-This is an auto-generated Django model module, created from the scanner schema with inspectdb,
-and then adapted to PEP8 and Django conventions.
+This was originally an auto-generated Django model module, created from the scanner schema with inspectdb and then adapted to PEP8 and Django conventions.  But as the project has progressed we have needed to expand it and improve naming conventions and data relationships.  Note that none
+of these models are showing primary key fields and that we have taken the default autoincrementing
+pk from Django for safety.
 '''
 
-class VulnerablePort(models.Model):
-    pvid = models.AutoField(primary_key=True)
-    protocol = models.CharField(max_length=150, db_column='Protocol')
-    port_number = models.IntegerField(db_column='PortNum')
-    description = models.TextField(db_column='VulnString', blank=True)
-    mitigation = models.TextField(db_column='FixString', blank=True)
+class KnownPort(models.Model):
+    '''
+    Represents a port known to AIVScan as harboring potential problems.
+    '''
+    # these members restrict the choices available to insert in the protocol field.
+    TCP= 'tcp'
+    UDP='udp'
+    ICMP='icmp'
+    PROTOCOLS = ((TCP,'tcp'), (UDP,'udp'),(ICMP,'icmp'))
+    protocol = models.CharField(max_length=4, choices=PROTOCOLS, blank=True)
 
-    class Meta:
-        app_label = 'scanner'
-        db_table = u'PortVulns'
+    port_number = models.IntegerField()
+    description = models.TextField(blank=True)
+    mitigation = models.TextField(blank=True)
+
 
 class KnownVulnerability(models.Model):
-    tvid = models.AutoField(primary_key=True)
-    script_id = models.CharField(max_length=300, db_column='ScriptID', db_index=True)
-    public_id = models.CharField(max_length=20, blank=True)
-    match_string = models.CharField(max_length=600, db_column='MatchString')
-    description = models.TextField(db_column='VulnString', blank=True)
-    mitigation = models.TextField(db_column='FixString', blank=True)
+    '''
+    Represents a vulnerability known to AIVScan via nmap .nse scripts.
+    '''
+    script_id = models.CharField(max_length=300, db_index=True)  # the .nse script used
+    public_id = models.CharField(max_length=20, blank=True) # the CVE or MS number
+    match_string = models.CharField(max_length=600)
+    description = models.TextField(blank=True)
+    mitigation = models.TextField(blank=True)
     risk_level = models.PositiveSmallIntegerField()
 
     class Meta:
-        app_label = 'scanner'
-        db_table = u'TextVulns'
         verbose_name_plural = 'known vulnerabilities'
-
-class Host(models.Model):
-    hid = models.AutoField(primary_key=True)
-    scan = models.ForeignKey('Scan', db_column='sid')
-    ip4 = models.TextField(blank=True)
-    hostname = models.TextField(blank=True)
-    status = models.TextField(blank=True)
-    mac = models.TextField(blank=True)
-    distance = models.IntegerField(null=True, blank=True)
-    uptime = models.TextField(blank=True)
-    last_boot = models.TextField(blank=True, db_column='upstr')
-
-    # unused fields
-    # tcpcount = models.IntegerField(null=True, blank=True)
-    # udpcount = models.IntegerField(null=True, blank=True)
-    # ip4num = models.IntegerField(null=True, blank=True)
-    # vendor = models.TextField(blank=True)
-    # ip6 = models.TextField(blank=True)
-
-    class Meta:
-        app_label = 'scanner'
-        db_table = u'hosts'
 
 
 class OperatingSystem(models.Model):
-    oid = models.AutoField(primary_key=True)
-    host = models.ForeignKey(Host, db_column='hid')
-    name = models.TextField(blank=True)
-    family = models.TextField(blank=True)
-    generation = models.TextField(blank=True)
-    os_type = models.TextField(blank=True, db_column='type')
-    vendor = models.TextField(blank=True)
+    '''
+    Represents the best guess by nmap as to the operating system of the scanned host.  A host has
+    one operating system.
+    '''
+    name = models.CharField(max_length=200, blank=True)
+    family = models.CharField(max_length=200, blank=True)
+    generation = models.CharField(max_length=200, blank=True)
+    os_type = models.CharField(max_length=200, blank=True)
+    vendor = models.CharField(max_length=200, blank=True)
 
-    # unused fields
+    # these fields are not used in this version of AIVScan
     # accuracy = models.IntegerField(null=True, blank=True)
 
-    class Meta:
-        app_label = 'scanner'
-        db_table = u'os'
 
+class Host(models.Model):
+    '''
+    Represents a single host scanned by nmap.  Must be associated with a scan.
+    '''
+    scan = models.ForeignKey('Scan', db_index=True)
+    ip4 = models.CharField(max_length=16, blank=True)
+    hostname = models.CharField(max_length=200, blank=True)
+    operating_system = models.ForeignKey(OperatingSystem, null=True)
+    mac = models.CharField(max_length=18, blank=True)
+    uptime = models.CharField(max_length=200, blank=True)
+    last_boot = models.CharField(max_length=200, blank=True)
 
-class Port(models.Model):
-    pid = models.AutoField(primary_key=True)
-    host = models.ForeignKey(Host, db_column='hid')
-    port_number = models.IntegerField(null=True, blank=True, db_column='port')
-    state = models.TextField(blank=True)
-    service_name = models.TextField(blank=True, db_column='name')
-    product = models.TextField(blank=True)
-    version = models.TextField(blank=True)
-    description = models.TextField(blank=True, db_column='extra')
-    proto = models.TextField(blank=True)
-    fingerprint = models.TextField(blank=True)
-
+    # these fields were part of the initial schema but are not used in this version of AIVScan
     # unused fields
-    # owner = models.TextField(blank=True)
-    # rpcnum = models.TextField(blank=True)
+    # status = models.CharField(max_length=200, blank=True)
+    # distance = models.IntegerField(null=True, blank=True)
+    # tcpcount = models.IntegerField(null=True, blank=True)
+    # udpcount = models.IntegerField(null=True, blank=True)
+    # ip4num = models.IntegerField(null=True, blank=True)
+    # vendor = models.CharField(max_length=200, blank=True)
+    # ip6 = models.CharField(max_length=200, blank=True)
+
+
+class FoundPort(models.Model):
+    '''
+    Represents a port discovered on a host.  Hosts have many ports, but a port exists only on one
+    host.
+    '''
+    host = models.ForeignKey(Host)
+
+    # these members restrict the choices available to insert in the protocol field.  Although having
+    # these defined both here and in the PortVulnerabilites class breaks the DRY principal, this is
+    # outweighed by the convenience of class-attribute access like Port.TCP
+    TCP= 'tcp'
+    UDP='udp'
+    ICMP='icmp'
+    PROTOCOLS = ((TCP,'tcp'), (UDP,'udp'),(ICMP,'icmp'))
+    protocol = models.CharField(max_length=4, choices=PROTOCOLS, blank=True)
+
+    # these members restrict the choices available to insert in the state field, and are the 6
+    # states recognized by nmap.
+    OPEN='open'
+    CLOSED='closed'
+    FILTERED='filtered'
+    UNFILTERED='unfiltered'
+    OPENFILTERED='open|filtered'
+    CLOSEDFILTERED='closed|filtered'
+    STATES = ((OPEN,OPEN),(CLOSED,CLOSED),(FILTERED,FILTERED),(UNFILTERED,UNFILTERED),
+              (OPENFILTERED,OPENFILTERED),(CLOSEDFILTERED,CLOSEDFILTERED))
+    state = models.TextField(max_length=16, choices=STATES, blank=True)
+
+    port_number = models.IntegerField(null=True, blank=True)
+    service_name = models.CharField(max_length=200, blank=True)
+    product = models.CharField(max_length=200, blank=True)
+    version = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    fingerprint = models.CharField(max_length=200, blank=True)
+
+    # these fields were part of the initial schema but are not used in this version of AIVScan
+    # owner = models.CharField(max_length=200, blank=True)
+    # rpcnum = models.CharField(max_length=200, blank=True)
     # confidence = models.IntegerField(null=True, blank=True)
-    # method = models.TextField(blank=True)
-    # tunnel = models.TextField(blank=True)
+    # method = models.CharField(max_length=200, blank=True)
+    # tunnel = models.CharField(max_length=200, blank=True)
     # port_type = models.TextField(blank=True, db_column='type')
 
-    class Meta:
-        app_label = 'scanner'
-        db_table = u'ports'
-
-
-class Scan(models.Model):
-    sid = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, db_column='userId') # Field name made lowercase.
-    subscription_level = models.IntegerField(null=True, blank=True)
-    version = models.TextField(blank=True)
-    nmap_args = models.TextField(blank=True, db_column='args')
-    start_time = models.DateTimeField(null=True, blank=True, db_column='starttime')
-    end_time = models.DateTimeField(null=True, blank=True, db_column='endtime')
-
-    # unused fields
-    # xml_version = models.TextField(blank=True)
-    # types = models.TextField(blank=True) # unused
-    # start_str = models.TextField(blank=True) # unused
-    # end_str = models.TextField(blank=True) # unused
-    # num_services = models.IntegerField(null=True, blank=True) # unused
-
-    class Meta:
-        app_label = 'scanner'
-        db_table = u'scans'
 
 class FoundVulnerability(models.Model):
-    vid = models.AutoField(primary_key=True)
-    host = models.ForeignKey(Host, db_column='hid')
-    known_vuln = models.ForeignKey(KnownVulnerability, db_column='tvid')
+    '''
+    Represents a KnownVulnerability that has been discovered on a host.  Found vulnerabilities
+    have one and only one associated known vulnerability.  Hosts can have many found
+    vulnerabilities.
+    '''
+    host = models.ForeignKey(Host)
+    known_vuln = models.ForeignKey(KnownVulnerability)
 
     class Meta:
-        app_label = 'scanner'
-        db_table = u'vulns'
         verbose_name_plural = 'found vulnerabilities'
 
 
+class Scan(models.Model):
+    '''
+    Represents a user-initiated nmap scan.  Users can have multiple scans, scans can have
+    multiple hosts in the database but are limited due to NAT to one for practical purposes.
+    '''
+    user_id = models.ForeignKey(User, db_index=True)
+    nmap_args = models.TextField()
+    start_time = models.DateTimeField(default=datetime.datetime.now())
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    # these members restrict the choices available to insert in the status field
+    PENDING = 'P'
+    COMPLETE = 'C'
+    STATUS = (( PENDING, 'pending'), (COMPLETE, 'complete'))
+    status = models.CharField(max_length=1, choices=STATUS, default=PENDING)
+
+    # these fields were part of the initial schema but are not used in this version of AIVScan
+    # subscription_level = models.IntegerField(null=True, blank=True)
+    # version = models.CharField(max_length=200, blank=True)
+    # xml_version = models.CharField(max_length=200, blank=True)
+    # types = models.CharField(max_length=200, blank=True)
+    # start_str = models.CharField(max_length=200, blank=True)
+    # end_str = models.CharField(max_length=200, blank=True)
+    # num_services = models.IntegerField(null=True, blank=True)
+
+
 class MockModel():
+    '''
+    Used as an empty class to safely add dynamic attributes to for purposes of creating default
+    objects at runtime.
+    '''
     pass
 
-admin.site.register(FoundVulnerability)
-admin.site.register(Scan)
-admin.site.register(Port)
+
+# register all the above models with the Django admin.
+admin.site.register(KnownVulnerability)
+admin.site.register(KnownPort)
 admin.site.register(OperatingSystem)
 admin.site.register(Host)
-admin.site.register(KnownVulnerability)
-admin.site.register(VulnerablePort)
+admin.site.register(FoundVulnerability)
+admin.site.register(FoundPort)
+admin.site.register(Scan)
+
+
+
